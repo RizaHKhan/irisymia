11cd1bce883752ae0f948dcb398d8c3f71f35260
khanr <khanriza@gmail.com>
2020-12-31 21:50:58 -0500

    added most of Footer items

---
+++ components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-footer padless>
+    <v-row justify="center" no-gutters>
+      <v-col xs="6" sm="6" md="2">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title
+              class="mb-3 text-subtitle-1 text-center font-weight-thin"
+              >{{ $t('footer.navigate.title') }}</v-list-item-title
+            >
+            <v-btn
+              v-for="(item, index) in navigate"
+              :key="index"
+              :to="item.link"
+              text
+              small
+              class="text-caption"
+              >{{ item.title }}</v-btn
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+      <v-col xs="6" sm="6" md="2">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title
+              class="mb-3 text-subtitle-1 text-center font-weight-thin"
+              >{{ $t('footer.categories.title') }}</v-list-item-title
+            >
+            <v-btn
+              v-for="(item, index) in categories"
+              :key="index"
+              :to="item.link"
+              text
+              small
+              class="text-caption"
+              >{{ item.title }}</v-btn
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+      <v-col cols="12" sm="6" md="2">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title
+              class="mb-3 text-subtitle-1 font-weight-thin text-center"
+              >{{ $t('footer.info.title') }}</v-list-item-title
+            >
+            <v-list-item
+              v-for="(item, index) in info"
+              :key="index"
+              v-html="item.title"
+            ></v-list-item>
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="mb-3 text-subtitle-1 font-weight-thin">{{
+              $t('footer.subscribe.title')
+            }}</v-list-item-title>
+            <v-list-item class="text-caption pa-0">
+              {{ $t('footer.subscribe.subtitle') }}
+            </v-list-item>
+            <SubscribeForm />
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+import SubscribeForm from '@/components/SubscribeForm'
+export default {
+  components: {
+    SubscribeForm,
+  },
+  data() {
+    return {
+      navigate: [
+        { title: this.$t('footer.navigate.blog'), link: '/blog' },
+        { title: this.$t('footer.navigate.sitemap'), link: '/sitemap' },
+      ],
+      categories: [
+        { title: 'Bandejitas', link: '/bandejitas' },
+        { title: 'Bombas de chocolate', link: '/cbombas' },
+        { title: 'Galletitas artensanles', link: '/galletitas' },
+        { title: 'Romos de fresas', link: '/romas' },
+        { title: 'Tabletas de chocolate', link: '/chocolate' },
+      ],
+      info: [
+        {
+          title: this.$t('footer.info.one'),
+        },
+        {
+          title: this.$t('footer.info.two'),
+        },
+        {
+          title: this.$t('footer.info.three'),
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ components/SubscribeForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      :label="$t('subscribe-form.email')"
+      required
+      @blur="checkToValidate"
+      outlined
+    ></v-text-field>
+
+    <v-btn color="grey darken-4 white--text" @click="submitForm" large>
+      {{ $t('subscribe-form.subscribe') }}
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    email: '',
+    emailRules: [
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    lazy: true,
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
+    submitForm() {
+      console.log('submitting the form')
+    },
+    checkToValidate() {
+      if (!this.email) {
+        this.$refs.form.resetValidation()
+      }
+    },
+  },
+}
+</script>
