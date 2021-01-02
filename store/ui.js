0edfaa4b038105668483adfab62fd16643fe0674
khanr <khanriza@gmail.com>
2021-01-02 18:12:30 -0500

    adjusted the method name

--- store/ui.js
+++ store/ui.js
@@ -1,13 +1,21 @@
 export const state = () => ({
   navigationBar: false,
+  loading: true,
 })
 
 export const mutations = {
-  toggleNavigationDrawer(state) {
+  TOGGLE_NAVIGATIONDRAWER(state) {
     state.navigationBar = !state.navigationBar
+  },
+  LOADING_TRUE(state) {
+    state.loading = true
+  },
+  LOADING_FALSE(state) {
+    state.loading = false
   },
 }
 
 export const getters = {
   navigationBarState: (state) => state.navigationBar,
+  loadingState: (state) => state.loading,
 }
