fadc67ec624ccb8310669aec339e578dfb25ff41
khanr <khanriza@gmail.com>
2021-01-02 14:41:22 -0500

    removed unneeded logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,7 @@
     <NavigationDrawer />
     <v-main>
       <v-container>
-        <v-row>
-          <v-col v-for="n in 24" :key="n" cols="4">
-            <v-card height="200"></v-card>
-          </v-col>
-        </v-row>
+        <nuxt />
       </v-container>
       <Footer />
     </v-main>
