980be46550b9d5b98a62b6a5d101ec7981b425de
khanr <khanriza@gmail.com>
2021-01-25 15:11:29 -0500

    reverted nuxt-link placement to be inside of card and also changed text color

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,25 +1,27 @@
 <template>
   <transition name="fade">
-    <nuxt-link :to="`/${categoryuid}/${product.uid}`">
-      <v-card
-        class="ma-3 d-flex flex-column justify-space-between grey lighten-5"
-        max-width="450"
-        width="100%"
-      >
+    <v-card
+      class="ma-3 d-flex flex-column justify-space-between grey lighten-5"
+      max-width="450"
+      width="100%"
+    >
+      <nuxt-link :to="`/${categoryuid}/${product.uid}`">
         <v-img height="250px" :src="product.data.image.url"> </v-img>
 
         <v-row class="d-flex px-2 justify-space-between">
-          <v-card-title class="text-h5 font-weight-light">{{
+          <v-card-title class="text-h5 font-weight-light black--text">{{
             product.data.name[0].text
           }}</v-card-title>
-          <v-card-title class="text-h5">${{ product.data.price }}</v-card-title>
+          <v-card-title class="text-h5 black--text"
+            >${{ product.data.price }}</v-card-title
+          >
         </v-row>
 
         <v-card-text class="text--primary mt-2">
           <div>{{ product.data.description[0].text }}</div>
         </v-card-text>
-      </v-card>
-    </nuxt-link>
+      </nuxt-link>
+    </v-card>
   </transition>
 </template>
 
